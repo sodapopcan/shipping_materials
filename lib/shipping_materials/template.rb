ea5807b433e085e00208a5c2bfa6ebc4d1960ffc
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-15 16:05:30 -0400

    Implement Template#layout_file

--- lib/shipping_materials/template.rb
+++ lib/shipping_materials/template.rb
@@ -1,46 +1,56 @@
 module ShippingMaterials
   require 'mustache'
 
-  attr_accessor :layout
-	attr_reader :rendered
-
   class Template < Mustache
+    attr_reader :header, :footer
+
     def initialize(objects, filename)
       @objects  = objects
-			@filename = filename
+      @filename = filename
       @rendered = ''
+			@header =''
+			@footer = ''
     end
 
-    def package
+    def layout_file=(name)
+      layout = File.read("#{template_path}/#{name}")
+      @header, @footer = layout.split(/{{yield}}/)
+    end
+
+    def render!
+			@rendered += @header
+
       @objects.each do |o|
         add_methods(o)
         @rendered << render
       end
+
+			@rendered += @footer
     end
 
     def object=(object)
       add_methods(object)
     end
 
-		def to_s
-			@rendered
-		end
+    def to_s
+      @rendered
+    end
 
-		def to_pdf
-			fhtml = "/tmp/#{File.basename(@filename)}.html"
-			fpdf = "~/#{File.basename(@filename)}.pdf"
-			File.open(fhtml, 'w') {|f| f.write(@rendered) }
-			%x( wkhtmltopdf #{fhtml} #{fpdf} )
-		end
+    def to_pdf
+      fhtml = "/tmp/#{File.basename(@filename)}.html"
+      fpdf = "~/#{File.basename(@filename)}.pdf"
+      File.open(fhtml, 'w') {|f| f.write(@rendered) }
+      %x( wkhtmltopdf #{fhtml} #{fpdf} )
+    end
 
 
     private
 
     def add_methods(object)
-			@public_methods ||= object.public_methods(false).grep(/[^=]$/)
-      @public_methods.each do |m|
-        self.class.send(:define_method, m) do
-          object.send(m)
+      @public_methods ||= object.public_methods(false).grep(/[^=]$/)
+      @public_methods.each do |meth|
+        self.class.send(:define_method, meth) do
+          object.send(meth)
         end
       end
     end
--- test/template_test.rb
+++ test/template_test.rb
@@ -4,17 +4,23 @@ class TemplateTest < UnitTest
   def setup
     @tpl = ShippingMaterials::Template.new(orders.slice(0,2), 'filename')
     @tpl.template_file = 'test/template.mustache'
+    @tpl.layout_file = 'test/layout.mustache'
+  end
+
+  def test_layout_file_setter
+    assert_equal '<html><body></body></html>', @tpl.header + @tpl.footer,
+      "Template header and footer not properly being set"
   end
 
   def test_template_render_and_concatination
-    @tpl.package
+    @tpl.render!
 
-    assert_equal "Andrew1\nJulie3\n", @tpl.to_s,
+    assert_equal "<html><body>Andrew1\nJulie3\n</body></html>", @tpl.to_s,
       "Templates are not rendering properly"
   end
 
   def test_to_pdf
-    @tpl.package
+    @tpl.render!
     @tpl.to_pdf
   end
 end
