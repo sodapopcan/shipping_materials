a007920c89e0b7e7cba6f8088112f7380f358e7d
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-18 08:56:36 -0400

    Add AssetUtils

---
+++ lib/shipping_materials/asset_utils.rb
@@ -0,0 +1,16 @@
+module ShippingMaterials
+  class AssetUltils
+		def write_asset(asset)
+			filename = "#{Config.save_path}/#{asset.filename}"
+			File.open(filename, 'w') {|f| f.write(asset.to_s) } 
+		end
+
+    def write_pdf(asset)
+			base = "#{Config.save_path}/#{asset.basename}"
+			html_file, pdf_file = base + '.html', base + '.pdf'
+      File.open(html, 'w') {|f| f.write(asset.to_s) }
+      %x( wkhtmltopdf #{html} #{pdf} )
+			File.unlink(html_file)
+    end
+  end
+end
