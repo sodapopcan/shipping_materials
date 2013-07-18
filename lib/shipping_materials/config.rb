b3aac07adf9cf86ff3ea5b1665638aec034d54eb
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-18 14:21:16 -0400

    Add Config::base_context

--- lib/shipping_materials/config.rb
+++ lib/shipping_materials/config.rb
@@ -3,6 +3,14 @@ module ShippingMaterials
     class << self
       attr_writer :save_path
 
+      def base_context
+        @base_context || :object
+      end
+
+      def base_context=(bc)
+        @base_context = bc.to_sym
+      end
+
       def save_path
         @save_path || '.'
       end
---
+++ lib/shipping_materials/packing_slips.rb
@@ -0,0 +1,21 @@
+module ShippingMaterials
+  class PackingSlips
+    def initialize(objects, template_file)
+      @objects  = objects
+      @template_file = template_file
+    end
+
+    def to_s
+      t = Mustache.new
+      t.template_file = @template_file
+      t[Config.base_context] = @objects
+      t.render
+    end
+
+    def to_pdf
+      write(:pdf, to_s)
+    end
+
+    alias_method :to_html, :to_s
+  end
+end
