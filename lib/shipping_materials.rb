114547dec95d073555a0aed8a45109cfd243efe3
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-18 09:08:26 -0400

    Create and require mixins directory

--- lib/shipping_materials.rb
+++ lib/shipping_materials.rb
@@ -1,4 +1,5 @@
-Dir["lib/shipping_materials/*.rb"].each {|f| require f }
+Dir["lib/shipping_materials/mixins/*.rb"].each {|f| require f }
+Dir["lib/shipping_materials/*.rb"       ].each {|f| require f }
 
 module ShippingMaterials
   def self.config
