f0fadf3fa8067ac8239c0df505dbbbf808a3728d
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-19 18:59:54 -0400

    Clean-up Packager test

--- test/shipping_materials/packager_test.rb
+++ test/shipping_materials/packager_test.rb
@@ -5,52 +5,7 @@ class PackagerTest < TestCase
     @packager = ShippingMaterials::Packager.new
   end
 
-  # def test_format
-  #   @packager.package(orders) { format :pdf => 'path/to/template' }
-
-  #   assert_block "Format should have type and template" do
-  #     @packager.format == :pdf && @packager.template == 'path/to/template'
-  #   end
-  # end
-
-  # def test_packaging_multiple_groups
-  #   @packager.package(orders) do
-  #     group 'canada_standard_post'
-  #     group 'united_states_ups'
-  #     group 'international_ups_expedited'
-  #   end
-
-  #   assert_equal 3, @packager.groups.size,
-  #               "Multiple groups are borked"
-  # end
-
-  # def test_group_filter
-  #   @packager.package orders do
-  #     group 'canada_standard_post' do
-  #       filter { shipping_method == 'std' && country == 'CA' }
-  #     end
-  #   end
-
-  #   assert_block "Shipping method didn't filter" do
-  #     @packager.groups.first.objects.size > 0
-  #   end
-  # end
-
-  # def test_group_labels
-  #   @packager.package orders do
-  #     group 'canada_standard_post' do
-  #       filter { shipping_method == 'std' && country == 'CA' }
-  #       labels(:extension => 'csv', :headers => true)
-  #     end
-  #   end
-
-  #   assert_block "Label options are not getting set" do
-  #     labels = @packager.groups.first.labels
-  #     labels.first.extension == 'csv' && labels.first.headers?
-  #   end
-  # end
-
-  def test_entire_dsl
+  def test_dsl
     ShippingMaterials.config do |config|
       config.save_path = 'test/files/'
     end
@@ -58,8 +13,8 @@ class PackagerTest < TestCase
     @packager.package orders do
       packing_slips pdf: './test/files/template.mustache'
 
-      sort {
-        rule(:line_items) { type == 'Vinyl' }
+      sort(:line_items) {
+        rule { type == 'Vinyl' }
       }
 
       group 'canada_standard_post' do
@@ -98,9 +53,5 @@ class PackagerTest < TestCase
         end
       end
     end
-
-    # assert_equal [:object, :line_items],
-    #             @packager.groups.first.labels.first.row_maps.keys,
-    #             "Row maps aren't getting keyed"
   end
 end
