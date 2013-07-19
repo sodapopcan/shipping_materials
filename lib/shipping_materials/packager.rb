ca3cb1174e2342cdda9aed624eeed900d766ab32
Andrew Haust <andrewwhhaust@gmail.com>
2013-07-19 11:58:20 -0400

    Refactor Packager
    Untested and doesn't work

--- lib/shipping_materials/packager.rb
+++ lib/shipping_materials/packager.rb
@@ -12,21 +12,41 @@ module ShippingMaterials
       @objects = objects
       instance_eval(&block)
 			@groups.each do |group|
-        basename = group.basename
         sort_group(group)
-        group.packing_slips.each {|ps| FileUtils.write_pdf(ps.to_s, basename) } 
-        group.labels.each {|l| FileUtils.write_asset(l.to_s, basename) }
+        create_packing_slips(group)
+        create_labels(group)
 			end
     end
 
-    def packing_slips(file_type=nil, template=nil)
-			@packing_slips ||= PackingSlips.new(file_type, template)
+    def packing_slips(options={})
+      file_type, template = options.first
+			@packing_slips ||= PackingSlips.new(objects, template)
 		end
 
-    def group(filename, &block)
-      group = Group.new(filename, @objects)
+    def group(basename, &block)
+      group = Group.new(basename, @objects)
       group.instance_eval(&block) if block
       @groups << group
+    end
+
+    private
+
+    def sort_group(group)
+      return unless self.sorters.nil?
+      group.sorters ||= self.sorters unless group.sorters.nil?
+      group.sort!
+    end
+
+    def create_packing_slips(group)
+      group.packing_slips.each do |packing_slip|
+        FileUtils.write_pdf(group.basename, packing_slip.to_s)
+      end 
+    end
+
+    def create_labels(group)
+      group.labels.each do |label|
+        FileUtils.write_asset(group.basename, label.to_s)
+      end
     end
   end
 end
